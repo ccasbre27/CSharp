36e764ecd0a6aa490f6de544f5db4eff5a1c1f01
ccasbre27 <ccasbre27@hotmail.com>
2015-07-02 15:15:24 -0600

    Variables

---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // se define una variable entera y se inicializa
+            int variableEntera = 12;
+
+            // se define una variable double y se inicializa
+            double variableDecimal = 12.5;
+
+            // se define una variable string y se inicializa
+            string variableCadenaCaracteres = "Soy una cadena";
+
+            // se define una variable booleana y se inicializa
+            bool variableBooleana = true;
+
+        }
+    }
+}
